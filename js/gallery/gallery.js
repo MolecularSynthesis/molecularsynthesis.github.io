55424069a2c63854ffda528460702faefffb1a57
frdagostino <francesco.dagostino@mail.polimi.it>
2016-11-07 15:59:36 +0100

    correzione funzione js

--- js/gallery/gallery.js
+++ js/gallery/gallery.js
@@ -1,18 +1,18 @@
 //aggiornamento1
 
 
-//aggiornamento2
+//aggiornamento2 src="slide/2/2concept1.png"
 function pro2() {
                 var img = document.getElementById("imgPanel2");
                 for (i=1; i<=20; i++) {
                     if (Number(img.title)==i) {
                         i++
-                        img.src = "imgs/mask" + i + ".png";
+                        img.src = "slide/2/2concept" + i + ".png";
                         img.title = i;
                         break;
                     }
                     else if (Number(img.title)==20) {
-                        img.src = "imgs/mask" + 1 + ".png";
+                        img.src = "slide/2/2concept" + 1 + ".png";
                         img.title = 1;
                         break;
                     }
@@ -23,12 +23,12 @@ function pre2() {
                 for (i=20; i>=1; i--) {
                     if (Number(img.title)==i) {
                         i--
-                        img.src = "imgs/mask" + i + ".png";
+                        img.src = "slide/2/2concept" + i + ".png";
                         img.title = i;
                         break;
                     }
                     else if (Number(img.title)==1) {
-                        img.src = "imgs/mask" + 20 + ".png";
+                        img.src = "slide/2/2concept" + 20 + ".png";
                         img.title = 20;
                         break;
                     }
