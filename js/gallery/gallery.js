3d06e715a05b1b5f85a3cfafccc523d5c194b859
frdagostino <francesco.dagostino@mail.polimi.it>
2016-11-07 17:20:04 +0100

    modifiche varie

--- js/gallery/gallery.js
+++ js/gallery/gallery.js
@@ -4,32 +4,38 @@
 //aggiornamento2 src="slide/2/2concept1.png"
 function pro2() {
                 var img = document.getElementById("imgPanel2");
+                var slideNumber = document.getElementById("slideNumber2");
                 for (i=1; i<=20; i++) {
                     if (Number(img.title)==i) {
                         i++
                         img.src = "slide/2/2concept" + i + ".png";
                         img.title = i;
+                        slideNumber.innerHTML = "slide" + i;
                         break;
                     }
                     else if (Number(img.title)==20) {
                         img.src = "slide/2/2concept" + 1 + ".png";
                         img.title = 1;
+                        slideNumber.innerHTML = 1;
                         break;
                     }
                 }
             }
 function pre2() {
                 var img = document.getElementById("imgPanel2");
+                var slideNumber = document.getElementById("slideNumber2");
                 for (i=20; i>=1; i--) {
                     if (Number(img.title)==i) {
                         i--
                         img.src = "slide/2/2concept" + i + ".png";
                         img.title = i;
+                        slideNumber.innerHTML = "slide" + i;
                         break;
                     }
                     else if (Number(img.title)==1) {
                         img.src = "slide/2/2concept" + 20 + ".png";
                         img.title = 20;
+                        slideNumber.innerHTML = 20;
                         break;
                     }
                 }
